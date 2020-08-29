d5c4d730ba3f832b4f57def4347100bbb557fb17
James McMahon <james@focusedlabs.io>
2020-08-28 21:55:31 -0500

    asdf and execute bit

--- create-docker.sh
+++ create-docker.sh
@@ -2,5 +2,5 @@
 
 ./gradlew clean -x check build
 docker build --tag memory-test .
-docker tag  memory-test:latest $1/memory-test:latest
+docker tag memory-test:latest $1/memory-test:latest
 docker push $1/memory-test:latest
