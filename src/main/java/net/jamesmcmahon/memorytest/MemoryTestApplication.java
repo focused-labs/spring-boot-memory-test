409f3d0093dc5f6896ddd62a881556885bdd2338
James McMahon <james@focusedlabs.io>
2020-08-28 20:22:47 -0500

    Spring Initializr

---
+++ src/main/java/net/jamesmcmahon/memorytest/MemoryTestApplication.java
@@ -0,0 +1,13 @@
+package net.jamesmcmahon.memorytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoryTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoryTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/jamesmcmahon/memorytest/MemoryTestApplicationTests.java
@@ -0,0 +1,13 @@
+package net.jamesmcmahon.memorytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoryTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
