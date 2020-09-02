8f7d6607667be8e4cb6fa3e7d1667f508ce5c250
James McMahon <james@focusedlabs.io>
2020-09-02 12:20:04 -0500

    Better param names

--- src/main/java/net/jamesmcmahon/memorytest/UseMemoryController.java
+++ src/main/java/net/jamesmcmahon/memorytest/UseMemoryController.java
@@ -19,9 +19,9 @@ import static net.jamesmcmahon.memorytest.Stats.printMemoryStats;
 public class UseMemoryController {
     @GetMapping("eat-memory")
     public ResponseEntity<Float> eatMemory(
-            @RequestParam("chunks") int chunks,
-            @RequestParam("size") int size) {
-        UseMemoryController.allocateBytes(chunks, size);
+            @RequestParam("steps") int steps,
+            @RequestParam("mbs") int mbs) {
+        UseMemoryController.allocateBytes(steps, mbs);
         return ResponseEntity.ok().body(getRuntime().freeMemory() / (float) GIGABYTE);
     }
 
@@ -31,15 +31,15 @@ public class UseMemoryController {
         return ResponseEntity.ok().build();
     }
 
-    private static List<byte[]> allocateBytes(int chunks, int chunkSize) {
+    private static List<byte[]> allocateBytes(int steps, int megabytes) {
         var list = new ArrayList<byte[]>();
-        for (int i = 0; i < chunks; i++) {
+        for (int i = 0; i < steps; i++) {
             System.out.println(
                     "Free Memory: " +
                             getRuntime().freeMemory() / (float) GIGABYTE +
                             "GB"
             );
-            list.add(new byte[chunkSize * MEGABYTE]);
+            list.add(new byte[megabytes * MEGABYTE]);
         }
         return list;
     }
